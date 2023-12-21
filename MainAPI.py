cc55a3be2624ada32aa81475cef0e2cc56e68a89
Pawan Kumar <spawankumar@users.noreply.github.com>
2023-12-21 20:43:50 +1100

    Create MainAPI.py

---
+++ MainAPI.py
@@ -0,0 +1,28 @@
+import requests
+from requests.auth import HTTPBasicAuth
+# For in house hosted API
+# Replace these values with your SecurityCenter information
+Tenable_SERVER = 'https://your-securitycenter-server'
+ACCESS_KEY = 'your-access-key'
+SECRET_KEY = 'your-secret-key'
+SCAN_ID = 1  # Replace with the ID of the scan you want to initiate
+
+# API endpoint for initiating a scan
+SCAN_ENDPOINT = f'{Tenable_SERVER}/rest/scan/{SCAN_ID}/launch'
+
+# Set up the headers and authentication
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+}
+auth = HTTPBasicAuth(ACCESS_KEY, SECRET_KEY)
+
+# Initiate the scan
+try:
+    response = requests.post(SCAN_ENDPOINT, headers=headers, auth=auth, verify=False)
+    response.raise_for_status()
+    print(f'Scan initiation successful. Response: {response.text}')
+except requests.exceptions.HTTPError as err:
+    print(f'Error initiating scan: {err}')
+except Exception as e:
+    print(f'An error occurred: {e}')
